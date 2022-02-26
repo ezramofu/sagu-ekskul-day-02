d77f3304d5f53bfe1f52513dd01c9e7f8d305921
ezramofu <91767244+ezramofu@users.noreply.github.com>
2022-02-26 11:31:50 +0900

    push project 03

---
+++ project03/comments.php
@@ -0,0 +1,13 @@
+<?php 
+    //this is my first single line comment in php
+    echo "single line comment using this '//' <br><br> ";
+
+    #this also a 
+    #single line comment
+    echo "single line comment using this '#' <br><br>";
+    /*this is 
+    multiple line comment
+    in php
+    */
+    echo "mupltiple line comment usin '/*...*/'";
+?>
